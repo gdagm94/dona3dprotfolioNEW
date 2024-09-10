b74b6f29874ee1acbb202d0f4e70b80bd51ddad5
donaxo <dona.ngock@yahoo.com>
2024-09-10 10:33:55 -0400

    Update main.js
    Three Path

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three/build/three.module.js';
 import * as dat from 'dat.gui';
 import gsap from 'gsap';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
