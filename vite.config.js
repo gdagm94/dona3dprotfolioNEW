3bda67c4f4dcec39463a71499990ed2e2f7ee74f
gdagm94 <gdagm94@gmail.com>
2024-09-11 16:07:13 -0400

    C
    C

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/dona3dprotfolioNEW/', // Keep the base path for GitHub Pages
+  base: '/', // Keep the base path for GitHub Pages
   build: {
     outDir: 'dist', // Output directory for the build
   },
