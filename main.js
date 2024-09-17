3a17c400515fe2d384862d5235f75669c35b6706
gdagm94 <gdagm94@gmail.com>
2024-09-17 12:42:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import * as THREE from './node_modules/three/build/three.module.js';
 import * as dat from './node_modules/dat.gui/build/dat.gui.js';
-import gsap from 'gsap';
+import gsap from './node_modules/gsap/index.js';
 import Stats from 'three/addons/libs/stats.module.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
