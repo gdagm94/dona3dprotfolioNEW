72b1ec4e9e216c0dbba8afeb3cb8651aa7ef810d
gdagm94 <gdagm94@gmail.com>
2024-09-17 11:52:44 -0400

    C

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css';
-import * as THREE from './node_modules/three/build/three';
-import * as THREE_ADDONS from 'three-addons';
+import * as THREE from './node_modules/three/build/three.module.js';
 import * as dat from 'dat.gui';
 import gsap from 'gsap';
 import Stats from 'three/addons/libs/stats.module.js';
